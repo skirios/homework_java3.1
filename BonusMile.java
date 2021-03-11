75b9a84d76cc92e22c0e526aeddec3d4c82301f6
skirios <skiriosfoto@gmail.com>
2021-03-11 13:05:51 +0300

    first commit

---
+++ BonusMile.java
@@ -0,0 +1,8 @@
+public class BonusMile {
+    public static void main(String[] args) {
+        int spentMoney = 150000;
+        int coefficient = 2000;
+        int bonusMile = spentMoney / coefficient;
+        System.out.println("Bonus Mile: " + bonusMile);
+    }
+}
